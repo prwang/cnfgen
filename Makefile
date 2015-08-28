02f2f30d62c67fa4a8a06ada7bfbac5cdd14b7b1
Massimo Lauria <lauria.massimo@gmail.com>
2015-08-28 11:38:52 +0200

    Improving the build process.

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@
 ########### CONFIGURATION ########################
 PROJECT="cnfgen"
 SOURCES=index.org transformation.org background.org graphformats.org
-EMACS=emacs
+EMACS=/usr/bin/emacs
 
 ifeq ($(shell uname -s),Darwin)
 BROWSE=open
@@ -17,7 +17,10 @@ endif
 
 TARGET= $(SOURCES:.org=.html)
 
-all: ${TARGET}
+all: version ${TARGET}
+
+version:
+	@$(EMACS) -Q -batch -l setup.el -f version-report >/dev/null
 
 clean:
 	@-rm -f  ${TARGET}
@@ -26,7 +29,6 @@ view: ${TARGET}
 	@${BROWSE} index.html
 
 %.html: %.org 
-	@echo "Convert org-files to html static files"
-	@$(EMACS) -Q -batch --visit=$< -l setup.el -f my-export-org 2>/dev/null >/dev/null
+	@$(EMACS) -Q -batch --visit=$< -l setup.el -f export-org-file >/dev/null
 
-.PHONY: all clean view
+.PHONY: all clean view version
--- setup.el
+++ setup.el
@@ -5,10 +5,32 @@
 ;;
 ;; $ emacs -Q -batch --visit=file.org -l setup.el -f my-export-org
 ;;
+;; The script requires a decent version of org-mode (say 8.x).
+;; Any Emacs 24.x should have it installed by default. For older emacs
+;; installations the script breaks. In particular the Emacs 22 shipped
+;; with MacOSX still has org-mode 4.6x.
+;;
+(require 'org)
 
-(defun my-export-org ()
-  (let ((org-html-preamble nil)
-        (org-html-postamble nil)
-        (org-html-toplevel-hlevel 3)
-        (org-html-head-include-default-style nil))
-    (org-html-export-to-html)))
+(setq org-html-preamble nil             ; Do not add a default HTML preamble
+      org-html-postamble nil            ; Do not add a default HTML postamble
+      org-html-toplevel-hlevel 3        ; Level 1 headings in ORG file become <H3> in HTML export.
+      org-html-head-include-default-style nil
+                                        ; No default CSS style
+      )
+
+
+(defun export-org-file ()
+  (message (format "\n--- %s ---\n" (file-name-nondirectory (buffer-file-name))))
+    (condition-case nil
+        (if (version-list-< (version-to-list org-version) '(8 0 0))
+            (org-export-as-html 1 nil nil t nil)
+          (org-html-export-to-html))
+      ('error
+       (message "ERROR: org-mode package is either missing or very old.")
+       )))
+
+
+(defun version-report ()
+  (message (format "EMACS: %s" (emacs-version)))
+  (message (format "ORG-MODE: %s (required >= 6.x)" org-version)))
